de6683f60260e86e0ad5aa48fec0db7be1aa3284
LuzikArbuzik <q64548@mvrht.com>
2017-05-02 18:02:18 +0200

    Działający ListAdapter.

--- app/src/main/java/com/example/jsonstatham/luzikarbuzik/FoodItemAdapter.java
+++ app/src/main/java/com/example/jsonstatham/luzikarbuzik/FoodItemAdapter.java
@@ -1,26 +1,21 @@
 package com.example.jsonstatham.luzikarbuzik;
 
 import android.content.Context;
-import android.support.annotation.LayoutRes;
-import android.support.annotation.NonNull;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
 import android.widget.TextView;
 
-public class FoodItemAdapter extends ArrayAdapter {
+public class FoodItemAdapter extends ArrayAdapter<String> {
 
-    private String[] foods;
-
-    public FoodItemAdapter(@NonNull Context context, String[] foods) {
-        super(context, 0);
-        this.foods = foods;
+    public FoodItemAdapter(Context context, String[] foods) {
+        super(context, 0, foods);
     }
 
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
-        String foodName = foods[position];
+        String foodName = getItem(position);
         if (convertView == null) {
             convertView = LayoutInflater.from(getContext()).inflate(R.layout.food, parent, false);
         }
