cf775d826debd432f3bb7a86e5549c4d94250fbb
LuzikArbuzik <q64548@mvrht.com>
2017-05-03 14:52:23 +0200

    Dodany ekran ze szczegółami.

--- app/src/main/java/com/example/jsonstatham/luzikarbuzik/Order.java
+++ app/src/main/java/com/example/jsonstatham/luzikarbuzik/Order.java
@@ -1,10 +1,13 @@
 package com.example.jsonstatham.luzikarbuzik;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 public class Order extends AppCompatActivity {
 
@@ -12,16 +15,18 @@ public class Order extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.order);
-        String[] food = new String[]{"Pizza Florentina", "Pizza Fiume", "Pizza Maffioso", "Pizza Frutti Di Mare",
+        final String[] food = new String[]{"Pizza Florentina", "Pizza Fiume", "Pizza Maffioso", "Pizza Frutti Di Mare",
                 "Pizza Marco Polo", "Pizza Giuseppe", "Pizza Uczta Sołtysa", "Pizza Jordano", "Pizza Vegetariana",
                 "Pizza Cosa Nostra"};
         FoodItemAdapter foodAdapter = new FoodItemAdapter(this, food);
         ListView list = (ListView)findViewById(R.id.food_list);
         list.setAdapter(foodAdapter);
-        list.setOnClickListener(new View.OnClickListener() {
+        list.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onClick(View v) {
-
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent newScreen = new Intent(getApplication(), OrderDetails.class);
+                newScreen.putExtra("foodName", food[position]);
+                startActivity(newScreen);
             }
         });
     }
--- app/src/main/java/com/example/jsonstatham/luzikarbuzik/OrderDetails.java
+++ app/src/main/java/com/example/jsonstatham/luzikarbuzik/OrderDetails.java
@@ -1,7 +1,9 @@
 package com.example.jsonstatham.luzikarbuzik;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 public class OrderDetails extends AppCompatActivity {
 
@@ -9,6 +11,9 @@ public class OrderDetails extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.order_details);
+        Intent input = getIntent();
+        TextView foodName = (TextView)findViewById(R.id.food_name);
+        foodName.setText(input.getStringExtra("foodName"));
     }
 
 }
