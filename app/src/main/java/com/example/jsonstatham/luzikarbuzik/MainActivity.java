f35bcea505e278ab2b989f41109736582cdcf240
LuzikArbuzik <q64548@mvrht.com>
2017-05-17 12:32:42 +0200

    Wysyłanie JSONa Stathama.

--- app/src/main/java/com/example/jsonstatham/luzikarbuzik/MainActivity.java
+++ app/src/main/java/com/example/jsonstatham/luzikarbuzik/MainActivity.java
@@ -15,6 +15,8 @@ import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.Callback;
 import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
+import retrofit2.converter.scalars.ScalarsConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,21 +28,20 @@ public class MainActivity extends AppCompatActivity {
 
    public void loginRestaurant(View button) {
       Retrofit retrofit = new Retrofit.Builder()
-               .baseUrl("http://127.0.0.1:8081/")
-               .build();
+              .baseUrl("http://127.0.0.1:8081/")
+              .addConverterFactory(ScalarsConverterFactory.create())
+              .addConverterFactory(GsonConverterFactory.create())
+              .build();
        Post post = retrofit.create(Post.class);
        Map<String, String> jsonParams = new ArrayMap<>();
        jsonParams.put("login", "1312");
        jsonParams.put("password", "Haslomaslo");
+       Call<String> response = post.login(new JSONObject(jsonParams).toString());
 
-       RequestBody body = RequestBody.create(okhttp3.MediaType.parse("application/json; charset=utf-8"),
-               (new JSONObject(jsonParams)).toString());
-       Call<ResponseBody> response = post.login(body);
-
-       response.enqueue(new Callback<ResponseBody>()
+       response.enqueue(new Callback<String>()
        {
            @Override
-           public void onResponse(Call<ResponseBody> call, retrofit2.Response<ResponseBody> rawResponse)
+           public void onResponse(Call<String> call, retrofit2.Response<String> rawResponse)
            {
                try
                {
@@ -52,7 +53,7 @@ public class MainActivity extends AppCompatActivity {
            }
 
            @Override
-           public void onFailure(Call<ResponseBody> call, Throwable throwable)
+           public void onFailure(Call<String> call, Throwable throwable)
            {
                Toast.makeText(getApplicationContext(), "Nie wysłano", Toast.LENGTH_LONG).show();
            }
--- app/src/main/java/com/example/jsonstatham/luzikarbuzik/Post.java
+++ app/src/main/java/com/example/jsonstatham/luzikarbuzik/Post.java
@@ -1,14 +1,18 @@
 package com.example.jsonstatham.luzikarbuzik;
 
+import org.json.JSONObject;
+
 import okhttp3.RequestBody;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.Headers;
 import retrofit2.http.POST;
 
 interface Post {
 
+    @Headers( "Content-Type: application/json" )
     @POST("/mule/login")
-    Call<ResponseBody> login(@Body RequestBody params);
+    Call<String> login(@Body String json);
 
 }
